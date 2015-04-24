86a641d23d3310a28707268c3c5f089e4f55ef0c
itsacezon <aecay@upd.edu.ph>
2015-04-25 03:53:28 +0800

    Crap

--- config.ru
+++ config.ru
@@ -1,3 +1,6 @@
 # encoding: UTF-8
+require 'sass/plugin/rack'
 require './fracht'
+
+use Sass::Plugin::Rack
 run Sinatra::Application
--- fracht.rb
+++ fracht.rb
@@ -6,6 +6,7 @@ require 'sinatra/content_for'
 require 'bcrypt'
 require 'slim'
 require 'pg'
+require 'sass'
 
 configure do
   set :database, ENV['HEROKU_POSTGRESQL_VIOLET_URL'] || 'postgres://localhost/upacmdb'
@@ -14,7 +15,31 @@ configure do
 
   ENV['TZ'] = 'Asia/Manila'
 
+  set :scss, {:style => :compressed, :debug_info => false}
+
   enable :sessions
   set :session_secret, '$2a$10$NYB.Sk0xxjDIpTvztIAn7.8Max3BXCLLnEzTSUZ3ghM2.PHAD3c0G'
   set :admins, [137, 140, 135, 136, 141]
+end
+
+get '/css/:name.css' do |name|
+  content_type :css
+  scss "sass/#{name}".to_sym, :layout => false
+end
+
+get '/' do
+  slim :shipments
+end
+
+get '/shipments' do
+  slim :shipments
+end
+
+get '/requests' do
+  slim :requests
+end
+
+get '/products' do
+  @products = Dir['public/img/products/*.jpg']
+  slim :products
 end
